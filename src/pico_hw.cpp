4dede47372677e1e0e568e3e7dca2a1d5782681f
Michi71 <47889385+Michi71@users.noreply.github.com>
2023-08-03 15:56:04 +0200

    Add files via upload

---
+++ src/pico_hw.cpp
@@ -0,0 +1,81 @@
+#include "pico_hw.h"
+#include "project_config.h"
+
+uint8_t u8x8_byte_pico_hw_i2c(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr)
+{
+    static uint8_t buffer[32]; /* u8g2/u8x8 will never send more than 32 bytes between START_TRANSFER and END_TRANSFER */
+    static uint8_t buf_idx;
+    uint8_t *data;
+
+    switch (msg)
+    {
+    case U8X8_MSG_BYTE_SEND:
+        data = (uint8_t *)arg_ptr;
+        while (arg_int > 0)
+        {
+            buffer[buf_idx++] = *data;
+            data++;
+            arg_int--;
+        }
+        break;
+    case U8X8_MSG_BYTE_INIT:
+        i2c_init(i2c1, 400*1000);
+        gpio_set_function(PIN_OLED_SDA, GPIO_FUNC_I2C);
+        gpio_set_function(PIN_OLED_SCL, GPIO_FUNC_I2C);
+        gpio_pull_up(PIN_OLED_SDA);
+        gpio_pull_up(PIN_OLED_SCL);
+        break;
+    case U8X8_MSG_BYTE_SET_DC:
+        break;
+    case U8X8_MSG_BYTE_START_TRANSFER:
+        buf_idx = 0;
+        break;
+    case U8X8_MSG_BYTE_END_TRANSFER:
+        i2c_write_blocking(i2c1, u8x8_GetI2CAddress(u8x8) >> 1, buffer, buf_idx, false);
+        break;
+    default:
+        return 0;
+        break;
+    }
+    return 1;
+}
+
+uint8_t u8x8_gpio_and_delay_pico(u8x8_t *u8x8, uint8_t msg,uint8_t arg_int, void *arg_ptr) 
+{
+  return 1;
+}
+
+
+
+void pico_init()
+{
+        // Overclock the CPU to 402 MHz.
+        vreg_set_voltage(VREG_VOLTAGE_1_30);
+        sleep_ms(1);
+        set_sys_clock_khz(402000, true);
+
+        // Initialize TinyUSB
+        board_init();
+        tusb_init();
+        stdio_init_all();
+        
+        // Initialize I2C
+       /* i2c_init(i2c1, 400*1000);
+        gpio_set_function(PIN_OLED_SDA, GPIO_FUNC_I2C);
+        gpio_set_function(PIN_OLED_SCL, GPIO_FUNC_I2C);
+        gpio_pull_up(PIN_OLED_SDA);
+        gpio_pull_up(PIN_OLED_SCL);*/
+	
+        // LED on GPIO25
+        gpio_init(PIN_LED);
+        gpio_set_dir(25, PIN_LED);
+
+        uint32_t rand_seed;
+        for (int i = 0; i < 32; i++)
+        {
+                bool randomBit = rosc_hw->randombit;
+                rand_seed = rand_seed | (randomBit << i);
+        }
+
+        srand(rand_seed);
+}
---
+++ src/pico_param_select.cpp
@@ -0,0 +1,162 @@
+#include "pico_userinterface.h"
+
+#include "mdaEPiano.h"
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+uint8_t pico_UserInterfaceParamSelect(u8g2_t *u8g2, Encoder *enc, PushButton *bt, mdaEPiano *ep)
+{
+	int32_t count = ep->getParameterCount();
+	
+	char buf[1024];
+	char label[24];
+	
+	u8g2_SetFont(u8g2, u8g2_font_8x13B_tf);	
+	
+	memset(buf, 0, 1024);
+	for (int i=0; i<count; i++) {
+		ep->getParameterName(i, label);		
+		strcat(buf, label);
+		strcat(buf, "\n");
+	}
+	strcat(buf, "<< BACK");
+	
+	int res = pico_UserInterfaceSelectionList(u8g2, enc, bt, "Parameter", 0, buf);
+	
+	if (res > count) res = -1;
+	return (res);	
+}
+
+
+uint8_t pico_UserInterfaceParamInput(u8g2_t *u8g2, Encoder *enc, PushButton *bt, mdaEPiano *ep, uint32_t paramIndex)
+{
+	uint8_t line_height;
+	uint8_t height;
+	u8g2_uint_t pixel_height;
+	u8g2_uint_t  y, yy;
+	u8g2_uint_t  pixel_width;
+	u8g2_uint_t  x, xx;
+
+	float local_value = ep->getParameter(paramIndex);
+	float old_value = local_value;
+	int32_t delta;
+	int32_t start;
+	
+	char title[24];
+	char label[24];
+	char displ[24];
+	
+	float lo;
+	float hi;
+	float step;
+	
+	ep->getParameterName(paramIndex, title);
+	ep->getParameterLabel(paramIndex, label);	
+	ep->getParameterDisplay(paramIndex, displ);	
+
+	lo = 0;
+	hi = 1;
+	step = 0.01;
+	
+	u8g2_SetFont(u8g2, u8g2_font_8x13B_tf);	
+	u8g2_SetFontDirection(u8g2, 0);
+	u8g2_SetFontPosBaseline(u8g2);
+	line_height = u8g2_GetAscent(u8g2);
+	line_height -= u8g2_GetDescent(u8g2);
+	height = 1;	/* value input line */
+	height += u8x8_GetStringLineCnt(title);
+	pixel_height = height;
+	pixel_height *= line_height;
+
+	y = 0;
+	if ( pixel_height < u8g2_GetDisplayHeight(u8g2)  )
+	{
+		y = u8g2_GetDisplayHeight(u8g2);
+		y -= pixel_height;
+		y /= 2;
+	}
+	
+	/* calculate offset from left for the label */
+	x = 0;
+	pixel_width = u8g2_GetUTF8Width(u8g2, displ);
+	pixel_width += u8g2_GetUTF8Width(u8g2, " ");
+	pixel_width += u8g2_GetUTF8Width(u8g2, label);
+	if ( pixel_width < u8g2_GetDisplayWidth(u8g2) )
+	{
+		x = u8g2_GetDisplayWidth(u8g2);
+		x -= pixel_width;
+		x /= 2;
+	}
+	
+	 /* event loop */
+	for(;;)
+	{
+		u8g2_FirstPage(u8g2);
+		do
+		{
+			if (old_value != local_value) {
+				ep->setParameter(paramIndex, local_value);
+				old_value = local_value;
+			}
+			ep->getParameterDisplay(paramIndex, displ);
+			
+			/* render */
+			yy = y;
+			yy += u8g2_DrawUTF8Lines(u8g2, 0, yy, u8g2_GetDisplayWidth(u8g2), line_height, title);
+			yy += 4;
+			xx = x;
+			xx += u8g2_DrawUTF8(u8g2, xx, yy, displ);
+			xx += u8g2_DrawUTF8(u8g2, xx, yy, " ");
+			u8g2_DrawUTF8(u8g2, xx, yy, label);			
+		} while( u8g2_NextPage(u8g2) );
+		
+		start = to_ms_since_boot(get_absolute_time());
+		
+		for(;;)
+		{
+			delta = enc->delta();
+			if (bt->ReadButton() == PushButton::PRESSED)
+			{
+				return 1;
+			}
+			else if (delta > 0)
+			{
+				start = to_ms_since_boot(get_absolute_time());
+				local_value += step;								
+				if ( local_value >= hi )
+					local_value = hi;
+				break;
+			}
+			else if (delta < 0)
+			{
+				start = to_ms_since_boot(get_absolute_time());
+				local_value -= step;
+				if ( local_value <= lo )
+					local_value = lo;				  
+				break;
+			}       			
+			
+			if (delta == 0) {
+				if (to_ms_since_boot(get_absolute_time()) - start > 5000)
+				{
+					return -1;	
+				}
+			}
+		
+		}
+	}
+  
+	/* never reached */
+	return 0;  
+}
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
