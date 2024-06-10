cc4304603f6f3a8ec447450f9e6c712100f281fc
Michi71 <47889385+Michi71@users.noreply.github.com>
2024-06-10 12:44:36 +0200

    Update project_config.h

--- include/project_config.h
+++ include/project_config.h
@@ -3,9 +3,15 @@
 
 #define PIN_MIDI_RX 5
 
-#define PIN_I2S_DOUT  9
-#define PIN_I2S_BCK   10
-#define PIN_I2S_WS    11
+// Pimoroni Pico Audio
+//#define PIN_I2S_DOUT  9
+//#define PIN_I2S_BCK   10
+//#define PIN_I2S_WS    11
+
+// Waveshare Pico Audio
+#define PIN_I2S_DOUT  26
+#define PIN_I2S_BCK   27
+#define PIN_I2S_WS    28
 
 #define  PIN_ENC_BTN  13
 #define  PIN_ENC_A    20
@@ -16,7 +22,7 @@
 #define PIN_OLED_SDA  2
 #define PIN_OLED_SCL  3
 
-#define PIN_POT_0     28
+//#define PIN_POT_0     28
 #define PIN_POT_1     29
 
 #endif // __PROJECT_CONFIG_H__
