8c7a2c7151b2e25e4c464968226576b1a54a520b
Michi71 <47889385+Michi71@users.noreply.github.com>
2024-11-28 13:14:46 +0100

    Update FreeRTOSConfig.h

--- include/FreeRTOSConfig.h
+++ include/FreeRTOSConfig.h
@@ -31,7 +31,6 @@
 #define configUSE_TICKLESS_IDLE                 0
 #define configUSE_IDLE_HOOK                     0
 #define configUSE_TICK_HOOK                     0
-#define configCPU_CLOCK_HZ                      402000000   // 133MHz for RP2040
 #define configTICK_RATE_HZ                      ( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES                    32
 #define configMINIMAL_STACK_SIZE                ( configSTACK_DEPTH_TYPE ) 512 
